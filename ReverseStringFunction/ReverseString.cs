76871d6bdfca94d2b5d521d7e42c93c3ebd40d94
Brad <brad.podmore@integration.works>
2022-12-09 12:05:54 +1300

    Removed LINQ

--- ReverseStringFunction/ReverseString.cs
+++ ReverseStringFunction/ReverseString.cs
@@ -1,29 +1,38 @@
-using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Linq;
+using System;
 
-namespace ReverseStringFunction
+namespace ReverseStringFunction;
+
+public static class ReverseString
 {
-    public static class ReverseString
+    [FunctionName(nameof(ReverseString))]
+    public static IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
     {
-        [FunctionName(nameof(ReverseString))]
-        public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        var text = req.Query["text"];
+
+        string responseText;
+        if (text.Count > 0 && string.IsNullOrWhiteSpace(text[0]))
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            var text = req.Query["text"].FirstOrDefault();
-
-            var responseText = string.IsNullOrWhiteSpace(text)
-                ? "Please add text as a query parameter with some text to be reversed"
-                : new string(text.Reverse().ToArray());
-
-            return new OkObjectResult(responseText);
+            var chars = new char[text[0].Length];
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = text[0][chars.Length - i - 1];
+            }
+            responseText = new string(chars);
         }
+        else
+        {
+            responseText = "Please add text as a query parameter with some text to be reversed";
+        }
+
+        return new OkObjectResult(responseText);
     }
 }
